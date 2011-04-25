38b0cb736b6507282a884f9814a4888858f01cb5
DFectuoso <santiago1717@gmail.com>
2011-04-24 18:20:28 -0700

    Simple templates

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import util, template
 
 class MainHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write('Hello world!')
+  def get(self):
+    self.response.out.write(template.render('templates/main.html', locals()))
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
-                                         debug=True)
-    util.run_wsgi_app(application)
+  application = webapp.WSGIApplication([
+      ('/', MainHandler),
+  ], debug=True)
+  util.run_wsgi_app(application)
 
 if __name__ == '__main__':
-    main()
+  main()
